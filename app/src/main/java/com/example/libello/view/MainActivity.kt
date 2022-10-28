3c6d2d89082ddfbfa1061a796c4010b471c8acde
Montoya086 <mon21552@uvg.edu.gt>
2022-10-27 23:24:13 -0600

    binding on main activity

--- app/src/main/java/com/example/libello/view/MainActivity.kt
+++ app/src/main/java/com/example/libello/view/MainActivity.kt
@@ -6,13 +6,16 @@ import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
 import com.example.libello.R
+import com.example.libello.databinding.ActivityMainBinding
+import com.example.libello.databinding.FragmentLoginBinding
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
+class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
-
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         // Retrieve NavController from the NavHostFragment
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
