9db122422ed7e8e41f6383352e9bb3c436ab0f3a
Francisco Castillo <cas21562@uvg.edu.gt>
2022-11-10 21:23:58 -0600

    Firebase Text Retrieving

--- app/src/main/java/com/example/libello/view/EditNoteFragment.kt
+++ app/src/main/java/com/example/libello/view/EditNoteFragment.kt
@@ -5,13 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.libello.databinding.FragmentEditNoteBinding
+import com.google.firebase.database.*
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
 
 class EditNoteFragment : Fragment() {
     private val args by navArgs<EditNoteFragmentArgs>()
     private var _binding: FragmentEditNoteBinding? = null
     private val binding get() = _binding!!
+    private lateinit var database: DatabaseReference
+    private var text: String = "Mondongo"
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -23,6 +30,25 @@ class EditNoteFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.noteNameText.text = "Content of "+args.noteName
+        // TO READ DATA
+        database = FirebaseDatabase.getInstance().getReference("Notes")
+
+        val textListener = object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                text = snapshot.child("2").child("Content").value.toString()
+                binding.noteNameText.text = text
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        }
+        database.addValueEventListener(textListener)
+
+        // TO SAVE NEW DATA
+        binding.floatingActionButton.setOnClickListener{
+
+        }
     }
 }
